4cc9f4d275a3b26c1f2ceadcd548ca72d14e0417
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-06-19 13:55:44 +0545

    Make Home Page Dynamic using service
    Co-Authored-By: Samir Thapaliya <43093117+samirthapaliya@users.noreply.github.com>
    Co-Authored-By: Shankar4919 <57608215+Shankar4919@users.noreply.github.com>

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+# Internal Improt
+from service.models import Services
+
 # Create your views here.
 def Home(request):
 
-    return render(request, 'homepage/index.html')
+    service = Services.objects.filter(is_active=True)
+    context = {
+        'services': service
+    }
+    return render(request, 'homepage/index.html', context)
