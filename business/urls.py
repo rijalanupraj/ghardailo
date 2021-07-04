a13c946bd40e3ce32b4f71753db435ff3d967e5a
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-07-04 17:59:50 +0545

    Set up template for business search page

--- business/urls.py
+++ business/urls.py
@@ -3,11 +3,14 @@ from django.urls import path
 
 # Internal Import
 from .views import(
-    BusinessRegistartionCreateView
+    BusinessRegistartionCreateView,
+    BusinessListPageView
 )
 
 
 urlpatterns = [
     path('register/', BusinessRegistartionCreateView.as_view(),
          name='business-registration'),
+    path('business/', BusinessListPageView.as_view(), name='business-list-page')
+
 ]
--- business/views.py
+++ business/views.py
@@ -1,12 +1,22 @@
 # Extrenal Import
-from django.shortcuts import render
-from django.views.generic import CreateView
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from django.urls import reverse_lazy
 
 # Internal Import
 from django.contrib.auth import get_user_model
 from .forms import BusinessRegistrationForm
-
+from .models import Business
 User = get_user_model()
 
 
@@ -15,3 +25,20 @@ class BusinessRegistartionCreateView(CreateView):
     form_class = BusinessRegistrationForm
     template_name = 'business/business-registration.html'
     success_url = reverse_lazy('customer-home')
+
+
+class BusinessListPageView(UserPassesTestMixin, ListView):
+    template_name = "business/business-list-page.html"
+
+    # For Future Use
+    def test_func(self):
+        # if self.request.user.is_authenticated:
+        #     return not self.request.user.is_staff
+        return True
+
+    # def handle_no_permission(self):
+    #     return redirect('where you want to redirect')
+
+    def get_queryset(self):
+        request = self.request
+        return Business.objects.all()
