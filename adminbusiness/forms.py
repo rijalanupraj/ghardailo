2af2aa07b3ff65dee2d779d2a4de286f658536ee
Samir Thapaliya <43093117+samirthapaliya@users.noreply.github.com>
2021-07-12 12:25:17 +0545

    added form and views in businessadmin dashboard

---
+++ adminbusiness/forms.py
@@ -0,0 +1,57 @@
+from django.forms import ModelForm
+from business.models import *
+from gallery.models import *
+from worker.models import *
+from customer.models import *
+from service.models import *
+from hiring.models import *
+from review.models import *
+from notification.models import *
+
+
+class ServicesForm(ModelForm):
+    class Meta:
+        model = Services
+        fields = '__all__'
+
+class BusinessForm(ModelForm):
+    class Meta:
+        model = Business
+        fields = "__all__"
+        exclude = ['user','slug',]
+
+class GalleryForm(ModelForm):
+    class Meta:
+        model = Gallery
+        fields = "__all__"
+
+class WorkerForm(ModelForm):
+    class Meta:
+        model = Worker
+        fields = '__all__'
+
+
+class BusinessServicesForm(ModelForm):
+    class Meta:
+        model = Business_Service
+        fields = '__all__'
+
+class CustomerForm(ModelForm):
+    class Meta:
+        model = Customer
+        fields = "__all__"
+
+class HiringForm(ModelForm):
+    class Meta:
+        model = Services
+        fields = '__all__'
+
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = "__all__"
+
+class NotificationForm(ModelForm):
+    class Meta:
+        model = Notification
+        fields = '__all__'
--- adminbusiness/views.py
+++ adminbusiness/views.py
@@ -1,8 +1,45 @@
+from business.models import Business
 from django.shortcuts import render
+from business.models import *
+from gallery.models import *
+from worker.models import *
+from customer.models import *
+from service.models import *
+from hiring.models import *
+from review.models import *
+from notification.models import *
 
 # Create your views here.
 def businessDashboard(request):
-    return render(request, 'adminbusiness/base/dashboard.html')
+    business = Business.objects.all()
+    gallery = Gallery.objects.all()
+    business_service = Business_Service.objects.all()
+    business_service_count = Business_Service.objects.all().count()
+    worker = Worker.objects.all()
+    worker_count = Worker.objects.all().count()
+    customer = Customer.objects.all()
+    customer_count = Customer.objects.all().count()
+    hiring = Hiring.objects.all()
+    hiring_count = Hiring.objects.all().count()
+    review = Review.objects.all()
+    review_count = Review.objects.all().count()
+
+    context={
+        'business':business,
+        'gallery':gallery,
+        'business_service':business_service,
+        'business_service_count':business_service_count,
+        'worker':worker,
+        'worker_count':worker_count,    
+        'customer':customer,
+        'customer_count':customer_count,
+        'hiring':hiring,
+        'hiring_count':hiring_count,
+        'review':review,
+        'review_count':review_count,
+    }
+
+    return render(request, 'adminbusiness/base/dashboard.html', context)
 
 def editProfile(request):
     return render(request, 'adminbusiness/base/edit-profile.html')
