b65d36be2e791c9f97e48e65ae6fa3d4b2493fdc
Samir Thapaliya <43093117+samirthapaliya@users.noreply.github.com>
2021-07-12 15:19:02 +0545

    added url for service part
    shankar and samir

--- adminbusiness/urls.py
+++ adminbusiness/urls.py
@@ -4,6 +4,13 @@ from . import views
 urlpatterns = [
     path('dashboard', views.businessDashboard, name="businessDash"),
     path('edit-profile', views.editProfile, name="editProfileBus"),
-    path('edit-profile', views.editProfile, name="editProfileBus"),
+
+    # for service
+    path('getService',views.getService, name="getServiceDash"),
+    path('postService',views.postService, name="postServiceDash"),
+    path('updateService/<int:service_id>', views.updateService, name="updateServiceDash"),
+    path('deleteService/<int:service_id>', views.deleteService, name="postServiceDash"),
+
+
  
 ]
