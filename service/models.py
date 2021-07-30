37d81501cc969e83791b82bdd69f912301f2a4fd
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-07-30 14:38:02 +0545

    Make servicetag null in service modal

--- service/models.py
+++ service/models.py
@@ -13,11 +13,11 @@ class ServiceTag(models.Model):
 
 class Services(models.Model):
     icon_text = models.CharField(max_length=100, null=True)
-    color = models.CharField(max_length=100, null=True) 
+    color = models.CharField(max_length=100, null=True)
     name = models.CharField(max_length=100, null=False)
     is_active = models.BooleanField(default=True)
     description = models.TextField(blank=True)
-    servicetag = models.ManyToManyField(ServiceTag, blank=True)
+    servicetag = models.ManyToManyField(ServiceTag, blank=True, null=True)
 
     def __str__(self):
         return self.name
