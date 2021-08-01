8e2457b2fb4924ca8e0b1f81016f68a6a69381a3
1RANJITKARKI <60956570+1RANJITKARKI@users.noreply.github.com>
2021-08-01 18:34:39 +0545

    Changed Mysql Database to DBsqlite 3
    Changed MySQL Database to dBs lite 3

--- ghardailo/settings.py
+++ ghardailo/settings.py
@@ -86,12 +86,8 @@ WSGI_APPLICATION = 'ghardailo.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ghardailoproject',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'USER': 'root',
-        'PASSWORD': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 # Password validation
