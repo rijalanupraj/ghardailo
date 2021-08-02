547661e4b8582d3b3353b117472ce5a9b175666c
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-08-02 11:46:19 +0545

    Show Hiring Data in Customer Profile

--- customer/views.py
+++ customer/views.py
@@ -11,47 +11,49 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 
-
 # Internal Import
 
 from django import forms
 from .forms import CustomerUpdateForm
+from hiring.models import Hiring
 from django.contrib.auth import get_user_model
 from .models import Customer
 
 User = get_user_model()
 
 
-
-#@login_required(login_url='/')
+# @login_required(login_url='/')
 
 def profileupdate(request):
-      cu_form = CustomerUpdateForm
+    cu_form = CustomerUpdateForm
 
-      if request.method== 'POST':
+    if request.method == 'POST':
 
-         cu_form = CustomerUpdateForm(request.POST,request.FILES,instance=request.user.customer)
+        cu_form = CustomerUpdateForm(
+            request.POST, request.FILES, instance=request.user.customer)
 
-         if cu_form.is_valid():
-              cu_form.save()
-              messages.success(request, f' Your Account Has Been Successfully Updated')
-              return redirect('customerprofile')
+        if cu_form.is_valid():
+            cu_form.save()
+            messages.success(
+                request, f' Your Account Has Been Successfully Updated')
+            return redirect('customerprofile')
 
-      else:
-          
-          cu_form= CustomerUpdateForm(instance=request.user.customer)
+    else:
 
-      context= {
-            'cu_form': cu_form,
-      }
-     
+        cu_form = CustomerUpdateForm(instance=request.user.customer)
 
-      return render(request, 'customer/customerprofile.html', context)
+    # Hiring Part
+    customer_hire = Hiring.objects.filter(
+        customer=request.user.customer).order_by('-date_time')
+    context = {
+        'cu_form': cu_form,
+        'customer_hire': customer_hire,
+    }
+
+    return render(request, 'customer/customerprofile.html', context)
 
 
-
-
-# For password change 
+# For password change
 
 def change_password(request):
     if request.method == 'POST':
@@ -62,11 +64,10 @@ def change_password(request):
             messages.success(request, 'Your password Has Updated Successfully')
             return redirect('change_password')
         else:
-            messages.error(request, 'Invalid Password. Retype Your Password Correctly')
+            messages.error(
+                request, 'Invalid Password. Retype Your Password Correctly')
     else:
         form = PasswordChangeForm(request.user)
     return render(request, 'customer/changepassword.html', {
         'form': form
     })
-
-
