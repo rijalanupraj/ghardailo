e757da9b6b10252385d8b45a988e9c9e7e5a07c1
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-08-21 20:50:43 +0545

    Change Review model

--- review/models.py
+++ review/models.py
@@ -3,23 +3,10 @@ from django.core.validators import *
 from customer.models import *
 from business.models import *
 
+
 class Review(models.Model):
-    zero = 0
-    one = 1
-    two = 2
-    three = 3
-    four = 4
-    five = 5
-    rating_status = (
-        (zero, 0),
-        (one, 1),
-        (two, 2),
-        (three, 3),
-        (four, 4),
-        (five, 5)
-    )
-    business=models.ForeignKey(Business, on_delete=models.CASCADE, null=True)
-    customer=models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
-    comment=models.CharField(max_length=200, null=True)
-    rating=models.IntegerField(choices=rating_status, default=zero, null=True)
+    business = models.ForeignKey(Business, on_delete=models.CASCADE, null=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
+    comment = models.CharField(max_length=350, null=True)
+    rating = models.FloatField(default=0, null=True)
     date_time = models.DateTimeField(auto_now_add=True, null=True)
