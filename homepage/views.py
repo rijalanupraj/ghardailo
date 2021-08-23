04c5c2eb5e4a6e6666dc4b3a71ea8416693767b6
Samir Thapaliya <43093117+samirthapaliya@users.noreply.github.com>
2021-08-23 18:09:15 +0545

    bug fix

--- homepage/views.py
+++ homepage/views.py
@@ -6,14 +6,6 @@ from service.models import Services
 # Create your views here.
 
 
-def Home(request):
-
-    service = Services.objects.filter(is_active=True)[:8]
-    context = {
-        'services': service,
-    }
-    return render(request, 'homepage/index.html', context)
-
 
 def main_homepage(request):
 
