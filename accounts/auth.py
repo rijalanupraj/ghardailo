7f202900e3217a53343c5f57ec14c3e289478a36
gaurishankar007 <57608187+gaurishankar007@users.noreply.github.com>
2021-08-26 21:11:14 +0545

    authentication updating

--- accounts/auth.py
+++ accounts/auth.py
@@ -6,11 +6,11 @@ def unauthenticated_user(view_function):
     def wrapper_function(request, *args, **kwargs):
         if request.user.is_authenticated:
             if request.user.is_staff:
-                return redirect('')
+                return redirect('/a/dashboard')
             elif request.user.is_business:
-                return redirect('')
+                return redirect('/b/dashboard')
             elif request.user.is_customer:
-                return redirect('')
+                return redirect('/')
         else:
             return view_function(request, *args, **kwargs)
     return wrapper_function
@@ -20,24 +20,30 @@ def admin_only(view_function):
     def wrapper_function(request, *args, **kwargs):
         if request.user.is_staff:
             return view_function(request, *args, **kwargs)
+        elif request.user.is_business:
+            return redirect('/b/dashboard')
         else:
-            return redirect('')
+            return redirect('/')
     return wrapper_function
 
 
 def business_only(view_function):
     def wrapper_function(request, *args, **kwargs):
-        if request.user.is_staff:
-            return redirect('')
+        if request.user.is_business:
+            return view_function(request, *args, **kwargs)        
+        elif request.user.is_staff:
+            return redirect('/a/dashboard')
         else:
-            return view_function(request, *args, **kwargs)
+            return redirect('/')
     return wrapper_function
 
 
 def customer_only(view_function):
     def wrapper_function(request, *args, **kwargs):
+        if request.user.is_customer:
+            return view_function(request, *args, **kwargs)      
         if request.user.is_staff:
-            return redirect('')
+            return redirect('/a/dashboard')
         else:
-            return view_function(request, *args, **kwargs)
+            return redirect('/b/dashboard')
     return wrapper_function
