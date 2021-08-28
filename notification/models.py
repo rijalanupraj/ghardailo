473b10fd06daf57348d02ad490d48fdae2411ec6
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-08-28 12:51:27 +0545

    Show notification

--- notification/models.py
+++ notification/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from business.models import *
 from customer.models import *
+from django.utils import timezone
+
 
 class Notification(models.Model):
-    business = models.ForeignKey(Business, on_delete=models.CASCADE, null=True)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
-    message = models.CharField(max_length=100, null=True)
-    read = models.BooleanField(default=False, null=True)
-    date = models.DateField(auto_now_add=True, null=True)
-    time = models.TimeField(auto_now_add=True, null=True)
+    to_user = models.ForeignKey(
+        User, related_name='notification_to', on_delete=models.CASCADE, null=True)
+    from_user = models.ForeignKey(
+        User, related_name='notification_from', on_delete=models.CASCADE, null=True)
+    message = models.CharField(max_length=255, null=True)
+    has_seen = models.BooleanField(default=False, null=True)
+    datetime = models.DateTimeField(default=timezone.now)
