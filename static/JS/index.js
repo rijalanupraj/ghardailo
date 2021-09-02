7a237b4da275b354284fb365ddba16684f9802c3
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-09-02 14:12:35 +0545

    Fix index.js minor error

--- static/JS/index.js
+++ static/JS/index.js
@@ -13,8 +13,8 @@ if (preloader) {
 var toastElList = [].slice.call(document.querySelectorAll(".toast"));
 var toastList = toastElList.map(function (toastEl) {
   return new bootstrap.Toast(toastEl).show();
-}
-                                
+});
+
 /**
  * Nav Notification
  */
