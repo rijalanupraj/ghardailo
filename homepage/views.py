d29d00733067de4299d7df7faaf39052b0ec9868
1RANJITKARKI <60956570+1RANJITKARKI@users.noreply.github.com>
2021-09-04 12:31:59 +0545

    Counter part done
    Counting services, ,customer and business done

--- homepage/views.py
+++ homepage/views.py
@@ -1,25 +1,48 @@
+from business.models import Business
+from testimonial.models import Testimonial
 from django.shortcuts import render
 
 # Internal Improt
 from service.models import Services
+from customer.models import Customer
 
 # Create your views here.
 
+def Home(request):
 
+    service = Services.objects.filter(is_active=True)[:8]
+    context = {
+        'services': service,
+    }
+    return render(request, 'homepage/index.html', context)
 
 def main_homepage(request):
 
     service = Services.objects.filter(is_active=True)[:6]
+    testimonials= Testimonial.objects.filter(is_active=True)[:9]
+    countservice= Services.objects.filter(is_active=True).count()
+    countbusiness= Business.objects.filter(is_active=True).count()
+    countcustomer= Customer.objects.all().count()
     context = {
         'services': service,
+        'testimonials':testimonials,
+        'countservice': countservice,
+        'countbusiness':countbusiness,
+        'countcustomer':countcustomer,
     }
+    
     return render(request, 'homepage/home-page.html',context)
+
 
 
 def about_us(request):
 
 
     return render(request, 'homepage/Aboutus.html')
+
+
+
+
 
 
 
