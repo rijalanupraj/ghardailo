f75702835569560f3ef456ca8c67db2a962d12db
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-09-06 19:39:33 +0545

    Show average rating and total reviews

--- review/models.py
+++ review/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.core.validators import *
 from customer.models import *
-from business.models import *
 
 
 class Review(models.Model):
-    business = models.ForeignKey(Business, on_delete=models.CASCADE, null=True)
+    business = models.ForeignKey(
+        'business.Business', on_delete=models.CASCADE, null=True)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
     comment = models.TextField(max_length=350, null=True)
     rating = models.IntegerField(default=0, null=True)
