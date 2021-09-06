02595dccfcd199e4d6d7ee9a87a047436131be88
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-09-06 13:25:09 +0545

    Make hiring practical and changing business-profile-page

--- hiring/views.py
+++ hiring/views.py
@@ -47,17 +47,41 @@ class CreateHireView(UserPassesTestMixin, View):
         user = User.objects.get(id=request.user.id)
         customer = user.customer
 
-        # Create New Hire
-        Hiring.objects.create(
-            business_service=business_service, customer=customer, customer_message=message_text)
+        previous_hirings = Hiring.objects.filter(
+            business_service=business_service, customer=customer, status='PE')
+        try:
+            same_service = Hiring.objects.get(
+                business_service__service=service, customer=customer, status='PE')
+        except Hiring.DoesNotExist:
+            same_service = None
 
-        slug_of_current_business = request.build_absolute_uri(
-            reverse('business-profile', args=(business.slug, )))
-        messages.success(
-            request, f'You have successfully requested <a href="{slug_of_current_business}">{business.name}</a> for {service} service. Thank You 🙏')
-        # Notification Part
-        notification_message = f"{user.customer.name} requested for service {service.name} "
-        Notification.objects.create(
-            to_user=business.user, from_user=user, title="Hire Request", message=notification_message, business_service=business_service)
+        customer_pending_hiring_count = Hiring.objects.filter(
+            customer=customer, status='PE').count()
+        print(customer_pending_hiring_count)
+
+        slug_of_hiring_page = request.build_absolute_uri(
+            reverse('customer:customer-hiring-page'))
+        if previous_hirings:
+            messages.warning(
+                request, f'You have already hired this service from this business</b>{business.name}</b>. Check your hiring <a href="{slug_of_hiring_page}">here</a>.')
+        elif same_service:
+            messages.warning(
+                request, f'You have already hired this service from <b>{same_service.business_service.business.name}</b>. Check your hiring <a href="{slug_of_hiring_page}">here</a>.')
+        elif customer_pending_hiring_count >= 3:
+            messages.warning(
+                request, f'You have already requested hire <b>{customer_pending_hiring_count}</b> times. You can\'t request more than 3 service at a time.')
+        else:
+            # Create New Hire
+            Hiring.objects.create(
+                business_service=business_service, customer=customer, customer_message=message_text)
+
+            slug_of_current_business = request.build_absolute_uri(
+                reverse('business-profile', args=(business.slug, )))
+            messages.success(
+                request, f'You have successfully requested <a href="{slug_of_current_business}">{business.name}</a> for {service} service. Thank You 🙏')
+            # Notification Part
+            notification_message = f"{user.customer.name} requested for service {service.name} "
+            Notification.objects.create(
+                to_user=business.user, from_user=user, title="Hire Request", message=notification_message, business_service=business_service)
 
         return redirect(request.META.get('HTTP_REFERER', 'customer-home'))
