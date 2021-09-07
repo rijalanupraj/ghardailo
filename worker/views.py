8f0881718e656acde5555539c09e64ab1a11d39a
Shankar4919 <57608215+Shankar4919@users.noreply.github.com>
2021-09-07 16:42:03 +0545

    profile_card

--- worker/views.py
+++ worker/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from .forms import WorkerProfileForm
+from django.contrib import messages
+from .models import *
 
 # Create your views here.
+
+def getProfile(request, worker_id):
+    profile = Worker.objects.get(id=worker_id)
+
+    context = {
+        'profile':profile,
+    }
+    return render(request, 'worker/wprofile.html', context)
+
+# def profileupdate(request):
+#     cu_form = WorkerProfileForm
+
+#     if request.method == 'POST':
+
+#         cu_form = WorkerProfileForm(
+#             request.POST, request.FILES, instance=request.user.customer)
+#         print(cu_form)
+
+#         if cu_form.is_valid():
+#             cu_form.save()
+#             messages.success(
+#                 request, f' Your Account Has Been Successfully Updated')
+#             return redirect('customer:customerprofile')
+
+#     else:
+
+#         cu_form = WorkerProfileForm(instance=request.user.customer)
+
+#     # Hiring Part
+#     customer_hire = Hiring.objects.filter(
+#         customer=request.user.customer).order_by('-date_time')
+#     context = {
+#         'cu_form': cu_form,
+#         'customer_hire': customer_hire,
+#     }
+
+#     return render(request, 'customer/customerprofile.html', context)
+
+
