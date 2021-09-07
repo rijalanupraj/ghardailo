f5ab32aa0b4b56588389fcbf45a53503fe86cdee
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-09-07 13:55:17 +0545

    Back to top button

--- static/JS/index.js
+++ static/JS/index.js
@@ -21,3 +21,27 @@ var toastList = toastElList.map(function (toastEl) {
 $(function () {
   $('[data-tooltip="tooltip"]').tooltip();
 });
+
+/**
+ * Back To Top
+ */
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $("#back-to-top").fadeIn();
+    } else {
+      $("#back-to-top").fadeOut();
+    }
+  });
+  // scroll body to 0px on click
+  $("#back-to-top").click(function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      400
+    );
+    return false;
+  });
+});
