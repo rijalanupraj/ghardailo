429676f131ccaaff7024868538d9af6aeecd001c
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-09-23 15:34:43 +0545

    Fix business page error

--- adminbusiness/forms.py
+++ adminbusiness/forms.py
@@ -17,35 +17,38 @@ class ServicesForm(ModelForm):
         fields = '__all__'
 
 
-
 class BusinessForm(ModelForm):
     class Meta:
         model = Business
         fields = "__all__"
-        exclude = ['user','slug',]
+        exclude = ['user', 'slug', ]
+
 
 class EditBusinessForm(ModelForm):
     class Meta:
         model = Business
-        fields = ["name", "logo","cover_picture", "is_solo"]
+        fields = ["name", "logo", "cover_picture", "is_solo"]
+
 
 class BusinessProfileForm(ModelForm):
     class Meta:
         model = Business_Profile
         fields = "__all__"
-        exclude = ['business']
+        exclude = ['business', 'experience_year',
+                   'happy_customers', 'service_provided', 'awards_won']
+
 
 class BusinessProfileForm1(ModelForm):
     class Meta:
         model = Business
         fields = ['contact_email']
-        
 
 
 class GalleryForm(ModelForm):
     class Meta:
         model = Gallery
         fields = "__all__"
+
 
 class WorkerForm(ModelForm):
     class Meta:
@@ -59,41 +62,48 @@ class BusinessServicesForm(ModelForm):
         fields = '__all__'
         exclude = ['business']
 
+
 class BusinessServicesForm1(ModelForm):
     class Meta:
         model = Business_Service
         fields = '__all__'
-        exclude = ['business','service']
+        exclude = ['business', 'service']
+
 
 class BusinessWorkerForm(ModelForm):
     class Meta:
         model = Worker
         fields = '__all__'
-        exclude= ['business','user']
-  
+        exclude = ['business', 'user', 'slug']
+
 
 class CustomerForm(ModelForm):
     class Meta:
         model = Customer
         fields = "__all__"
 
+
 class HiringForm(ModelForm):
     class Meta:
         model = Services
         fields = '__all__'
+
 
 class ReviewForm(ModelForm):
     class Meta:
         model = Review
         fields = "__all__"
 
+
 class NotificationForm(ModelForm):
     class Meta:
         model = Notification
         fields = '__all__'
 
+
 class WorkerCreationForm(ModelForm):
     Worker_name = forms.CharField(max_length=255, required=False)
+
     class Meta:
         model = User
-        fields = ('username', 'email','Worker_name')
+        fields = ('username', 'email', 'Worker_name')
